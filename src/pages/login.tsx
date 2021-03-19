24a60bbf6e338a654ab62f0e472f2525758203ca
Lucas Ferreira <ljnferreira@gmail.com>
2021-03-19 08:43:07 -0300

    chore: added /login route

---
+++ src/pages/login.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import styles from "../styles/pages/About.module.scss";
+import Navbar from "../components/Navbar";
+
+export default function About() {
+  return (
+    <>
+      <Head key="About">
+        <title>Login | Maanaim Barbacena</title>
+      </Head>
+      <div className={styles.container}>
+        <Navbar></Navbar>
+        <h1>Login</h1>
+      </div>
+    </>
+  );
+}
