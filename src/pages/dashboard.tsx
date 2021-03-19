f5dfc71aac27dafe9f0acc7d25fb1417eaf06b1d
Lucas Ferreira <ljnferreira@gmail.com>
2021-03-19 08:49:46 -0300

    chore: added /dashboard route

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import styles from "../styles/pages/About.module.scss";
+import Navbar from "../components/Navbar";
+
+export default function About() {
+  return (
+    <>
+      <Head key="Dashboard">
+        <title>Dashboard | Maanaim Barbacena</title>
+      </Head>
+      <div className={styles.container}>
+        <Navbar></Navbar>
+        <h1>Dashboard</h1>
+      </div>
+    </>
+  );
+}
