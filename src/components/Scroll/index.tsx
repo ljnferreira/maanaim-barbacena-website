20cca0ba9a8a7d034cd7a8ddae89e18367d7f47e
Lucas Ferreira <ljnferreira@gmail.com>
2021-07-05 16:25:38 -0300

    feat: created and styled two new components

---
+++ src/components/Scroll/index.tsx
@@ -0,0 +1,12 @@
+import { ArrowDownIcon } from "../../assets/react-icons";
+import styles from "./Scroll.module.scss";
+
+const Scroll: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <ArrowDownIcon />
+    </div>
+  );
+};
+
+export default Scroll;
